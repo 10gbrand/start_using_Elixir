6a3ab5ee02ce87fa2f36ce4b7c072f602e79464f
10gbrand <m@tengbrand.se>
2023-04-29 13:35:19 +0000

    Läser in träddata från fil

--- learn1/lib/learn1.ex
+++ learn1/lib/learn1.ex
@@ -1,18 +1,26 @@
-defmodule Learn1 do
-  @moduledoc """
-  Documentation for `Learn1`.
-  """
+defmodule Traddata do
 
-  @doc """
-  Hello world.
+  def trad(id, treeSpecies, diameter, height) do
+    trad = %{id: id, treeSpecies: treeSpecies, diameter: diameter, height: height}
+    trad
+  end
 
-  ## Examples
+  def tradToTradlist do
+    tradlist = []
+    tradlist = [trad(1, "Björk", 20, 21)| tradlist]
+    tradlist = [trad(2, "Björk", 17, 18)| tradlist]
+    tradlist = [trad(3, "Gran", 22, 21)| tradlist]
+    tradlist = [trad(4, "Gran", 24, 22)| tradlist]
+    tradlist
+  end
 
-      iex> Learn1.hello()
-      :world
+end
 
-  """
-  def hello do
-    :world
+defmodule JsonTest do
+  def get_json(filename) do
+    with {:ok, body} <- File.read(filename),
+         {:ok, json} <- Poison.decode(body), do: {:ok, json}
   end
 end
+
+x = JsonTest.get_json("data/traddata.json")
