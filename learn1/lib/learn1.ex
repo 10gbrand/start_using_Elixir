e6159273132dbbedf5cffdc35d5b22fe270071e0
Mattias Tengbrand <mattias@tengbrand.se>
2023-04-30 06:16:53 +0000

    Nu funkar det att omvandla json för träddata till en lista av struct för träden och en struct för provytan.

--- learn1/lib/learn1.ex
+++ learn1/lib/learn1.ex
@@ -1,26 +1,59 @@
+defmodule Trad do
+  defstruct [:ytId, :id, :treeSpecies, :diameter, :height]
+
+  def tradJson_to_struct(traddata) do
+    # Gör om listan på enskillda träd till struct
+    treeList = traddata["TreeList"]
+    ytId = traddata["Id"]
+
+    Enum.map(treeList, fn val ->
+      # val["Diameter"]
+      %Trad{
+        ytId: ytId,
+        id: val["Id"],
+        treeSpecies: val["TreeSpecies"],
+        diameter: val["Diameter"],
+        height: val["Height"]
+      }
+    end)
+  end
+end
+
 defmodule Traddata do
+  defstruct [:id, :areal, :easting, :northing, :treeList]
 
-  def trad(id, treeSpecies, diameter, height) do
-    trad = %{id: id, treeSpecies: treeSpecies, diameter: diameter, height: height}
-    trad
+  def traddataJson_to_struct(traddata) do
+    %Traddata{
+      id: traddata["Id"],
+      areal: traddata["Areal"],
+      easting: traddata["Easting"],
+      northing: traddata["Northing"]
+    }
   end
-
-  def tradToTradlist do
-    tradlist = []
-    tradlist = [trad(1, "Björk", 20, 21)| tradlist]
-    tradlist = [trad(2, "Björk", 17, 18)| tradlist]
-    tradlist = [trad(3, "Gran", 22, 21)| tradlist]
-    tradlist = [trad(4, "Gran", 24, 22)| tradlist]
-    tradlist
-  end
-
 end
 
-defmodule JsonTest do
+defmodule JsonFromFile do
   def get_json(filename) do
-    with {:ok, body} <- File.read(filename),
-         {:ok, json} <- Poison.decode(body), do: {:ok, json}
+    with {:ok, body} <- File.read(filename), {:ok, json} <- Poison.decode(body), do: {:ok, json}
   end
 end
 
-x = JsonTest.get_json("data/traddata.json")
+# x = JsonFromFile.get_json("data/traddata.json")
+
+defmodule Testar do
+  def test1 do
+    data =
+      JsonFromFile.get_json("data/traddata.json")
+      |> elem(1)
+      |> Traddata.traddataJson_to_struct()
+
+    data
+  end
+
+  def test2 do
+    data =
+      JsonFromFile.get_json("data/traddata.json")
+      |> elem(1)
+      |> Trad.tradJson_to_struct()
+  end
+end
